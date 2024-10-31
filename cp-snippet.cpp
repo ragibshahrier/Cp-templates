ae43cc3d4d977d44b8a871005c3125c2c88f9b21
Ragib Shahrier Bin Kabir <ragibshahrier2004123@gmail.com>
2024-10-31 13:55:16 +0600

    modify orderedset

--- cp-snippet.cpp
+++ cp-snippet.cpp
@@ -2,7 +2,14 @@
 // #include <ext/pb_ds/assoc_container.hpp> 
 // #include <ext/pb_ds/tree_policy.hpp> 
 using namespace std;
+
 // #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+
+//OR
+
+// template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// template <typename T, typename R> using o_map = tree<T, R, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
 // using namespace __gnu_pbds; 
 
 #define int long long
